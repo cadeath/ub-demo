dac8ddc24f3b7afc5339cd594805f6f7ab292576
James Zerdick <ecjmaquic@gmail.com>
2023-07-25 11:40:24 +0800

    Adding Bootstrap

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 <html>
 <head>
     <title>DEMO</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <?php
         $cookieValue = isset($_GET['cookie']) ? $_GET['cookie'] : null;
-        if(!($cookieValue)) {
+        if (!($cookieValue)) {
             $epochTime = time();
             $randomNumber = rand(100000, 999999);
             $cookieValue = $epochTime . $randomNumber;
@@ -12,25 +14,37 @@
     ?>
 </head>
 <body>
-    <h1>DEMO</h1>
-    <?php
-    if (isset($_POST['ip'])) {
-        $ip = $_POST['ip'];
-        exec("ping -c 4 $ip", $output, $return_var);
+    <div class="container">
+        <h1 class="mt-3">DEMO</h1>
+        <?php
+        if (isset($_POST['ip'])) {
+            $ip = $_POST['ip'];
+            exec("ping -c 4 $ip", $output, $return_var);
 
-        // Display the ping result
-        echo "<pre>";
-        foreach ($output as $line) {
-            echo htmlspecialchars($line) . "<br>";
+            // Display the ping result
+            echo '<div class="mt-4">';
+            echo '<h3>Ping Result</h3>';
+            echo '<pre>';
+            foreach ($output as $line) {
+                echo htmlspecialchars($line) . "<br>";
+            }
+            echo '</pre>';
+            echo '</div>';
         }
-        echo "</pre>";
-    }
-    ?>
-    <form method="post">
-        <label for="ip">Enter IP Address:</label>
-        <input type="text" id="ip" name="ip" required>
-        <button type="submit">Ping</button>
-        <input type="hidden" name="cookie" value="<?php echo $cookieValue ?>" />
-    </form>
+        ?>
+        <form class="mt-4" method="post">
+            <div class="form-group">
+                <label for="ip">Enter IP Address:</label>
+                <input type="text" class="form-control" id="ip" name="ip" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Ping</button>
+            <input type="hidden" name="cookie" value="<?php echo $cookieValue ?>" />
+        </form>
+    </div>
+
+    <!-- Bootstrap JS and jQuery (for some components) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
